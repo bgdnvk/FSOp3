c76528af484add018538bf73e9193913f98daa15
bgdnvk <bnovykov@gmail.com>
2020-12-26 01:50:08 +0100

    adding heroku

--- phonebook backend/index.js
+++ phonebook backend/index.js
@@ -112,7 +112,11 @@ app.post('/api/persons', (req, res) => {
 // })
 
 //morgan impl
-const PORT = 3001
-app.listen(3001, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+// const PORT = 3001
+// app.listen(3001, () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
